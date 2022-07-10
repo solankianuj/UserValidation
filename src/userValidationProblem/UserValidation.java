3a2ab09dd2f2053121d34a30dbb14a6b560ce57e
solanki <anujaj2222.gmail.com>
2022-07-10 23:35:33 +0530

    Throwing Custom Exception In Case Of Invalid User Details.

--- src/userValidationProblem/UserValidation.java
+++ src/userValidationProblem/UserValidation.java
@@ -1,103 +1,110 @@
 package userValidationProblem;
 
+import java.util.Scanner;
+
 public class UserValidation {
-	
-	public boolean validateFirstName( String fname) {
+	Scanner scan =new Scanner(System.in);
+	public boolean validateFirstName() throws UserValidationException {
 		System.out.println("Enter User First Name.\n"+"Note-> Name Start With Capital And Has Minimum Three Character. ");
+		 String fname=scan.next();
 		
 		String regix="^[A-Z]{1,}"+"[a-z A-Z 0-9 ~!@#$%^&*?]{2,}";
-		
 		boolean result=fname.matches(regix);
-		if(result) {
-			System.out.println(fname+"  Is Valid. ");
+		try{
+			if(result) {
+				System.out.println(fname+"  Is Valid. ");
 			return true;
 		}
-		else
-			System.out.println(fname+" Is Not Valid.");
+		throw new UserValidationException("Invalid Enterd First Name");
+		}catch (UserValidationException e) {
+			System.out.println(e);
+		}
 		return false;
 		
 	}
 	
-	public  boolean validatingLastName(String lname) {
+	public  boolean validatingLastName() throws UserValidationException {
 		System.out.println("Enter User Last Name.\n"+"Note-> Name Start With Capital And Has Minimum Three Character. ");
-		
+		String lname=scan.next();
 		String regix="^[A-Z]{1,}"+"[a-z A-Z 0-9 ~!@#$%^&*?]{2,}";
 		
 		boolean result=lname.matches(regix);
+		try{
 		if(result) {
 			System.out.println(lname+"  Is Valid. ");
 			return true;
 		}
-		else
-		{
-			System.out.println(lname+" Is Not Valid.");
+		throw new UserValidationException("Invalid Enterd Last Name");
+		}catch (UserValidationException e) {
+			System.out.println(e);
 		}
 		return false;
-		
 	}
 	
-	public  boolean validatingEmail(String email) {
+	public  boolean validatingEmail() throws UserValidationException{
 		
 		System.out.println("Enter User Email Address.\n"+"Note-> ex.- abc.xyz@bl.co.in");
+		String email=scan.next();
 				
 		String regix="^([a-z]*)"+"[-_.+]*?"+"([a-z 0-9 ]*?)"+"[@]{1}"+"([a-z 0-9]{1,})"+"[.]{1}"+"([a-z]{2,})"+"[.]?"+"([a-z]*?)";
 		
 		boolean result=email.matches(regix);
-
+		try{
 		if(result)
 		{
 			System.out.println(email+"  Is Valid. ");
 			return true;
 		}
-		else
-		{
-			System.out.println(email+" Is Not Valid.");
-			return false;
-			}
+		throw new UserValidationException("Invalid Email");
+		}catch (UserValidationException e) {
+			System.out.println(e);
+		}
+		return false;
 		
 	}
 	
-		public  boolean validatingMobileNumber(String moNo) {
+		public  boolean validatingMobileNumber() throws UserValidationException {
 			
 			System.out.println("Enter User Mobile Number\n"+"In this Formate -: xx x98xxxx988" );
+			String moNo=scan.next();
 			
 			String regix="^[0-9]{2}"+" "+"[0-9]{10}";
 			
 			boolean result=moNo.matches(regix);
-
+			try{
 			if(result) {
 				System.out.println(moNo+"  Is Valid. ");
 				return true;
 			}
-			else
-			{
-				System.out.println(moNo+" Is Not Valid.");
-				return false;
+			throw new UserValidationException("Invalid Mobile Number");
+			}catch (UserValidationException e) {
+				System.out.println(e);
 			}
-			
+			return false;
 		}
 	
-	public  boolean validatingPassword(String pswd) {
+	public  boolean validatingPassword() throws UserValidationException{
 			
 			System.out.println("Enter User Password\n"+"Note-: Having Minimum 8 character with Atleast One Upper Case, One Numeric Number,One special Character." );
-						
+			String pswd=scan.next();			
 			String regix="^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*?/+])[A-Z a-z 0-9 -_.]{8,}";
 			
 			boolean result=pswd.matches(regix);
-
+			
+			try{
 			if(result) {
 				System.out.println(pswd+"  Is Valid. ");
 				return true;
 				
 			}
-			else
-			{
-				System.out.println(pswd+" Is Not Valid.");
-				return false;
+			throw new UserValidationException("Invalid Password");
+			}catch (UserValidationException e) {
+				System.out.println(e);
 			}
+			return false;
 	}
 	
-	public String moodAnalyser(String mood) {
+	public String moodAnalyser(String mood)  {
 		
 		if(mood.contains("sad")) {
 			return "Sad";
@@ -106,10 +113,18 @@ public class UserValidation {
 			return "Happy";
 		}
 
-		
+	
 	}
 	
-	
+	public static void main(String[] args) throws UserValidationException {
+		UserValidation userValidation=new UserValidation();
+		
+			userValidation.validateFirstName();
+			userValidation.validatingLastName();
+			userValidation.validatingMobileNumber();
+			userValidation.validatingEmail();
+			userValidation.validatingPassword();
+	}
 	
 	
 	
